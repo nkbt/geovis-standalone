f726615cd3c4d345f6b3443915df519ae26b2937
Nik Butenko <nik@butenko.me>
2016-09-10 14:19:39 +1000

    Only add attacks when playing

--- app/connect.js
+++ app/connect.js
@@ -1,5 +1,5 @@
 const websocket = require('ws');
-const {attacks} = require('@nkbt/geovis');
+const {attacks, controls} = require('@nkbt/geovis');
 
 const {
   WS_PORT = 10000
@@ -9,7 +9,9 @@ const {
 module.exports = store => {
   const onMessage = function (raw) {
     const message = JSON.parse(raw);
-    store.dispatch({type: attacks.ATTACKS_ADD, attacks: message.res});
+    if (store.getState().controls === controls.STATE_PLAYING) {
+      store.dispatch({type: attacks.ATTACKS_ADD, attacks: message.res});
+    }
   };
 
 
